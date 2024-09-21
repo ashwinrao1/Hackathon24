a218596f02e4f21a4d2ec863c7377a1f9901ac52
Demetris Chrysostomou <demetrischrysostomou1@gmail.com>
2024-09-21 13:02:59 -0500

    added date function

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ import pygame
 import sys
 import datetime
 
-def new_ingame_day():
-    return None
-
+def new_ingame_day(date):
+    date += datetime.timedelta(days=1)
+    return date
 
 pygame.init()
 
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+
 # initialize a screen w/ correct dimensions for display
-screen = pygame.display.set_mode((1200, 700))
+screen_width = 1200
+screen_height = 700
+screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption('Hackathon Game - Date Function')
 
@@ -32,13 +38,12 @@ while running:
     # EXAMPLE: MOVING OBJECTS, COLLISION DETECTION, ETC. 
 
     # clear the screen (make it black)
-    screen.fill((0,0,0))
+    screen.fill(WHITE)
 
     # ADD YOUR OWN CODE TO DRAW EVERYTHING ON THE SCREEN HERE
     # EXAMPLE: screen.blit(your_surface, (x, y)) FOR DISPLAYING IMAGES
 
-    # update the screen display
-    pygame.display.flip()
+
 
     # set limit on num frames per second game will render (cap the frame rate)
     
@@ -47,9 +52,25 @@ while running:
 
     total_time += delta_seconds
 
-    if total_time >= 60:
-        total_time -= 60
-        new_ingame_day()
+    seconds_for_day = 60
+
+    if total_time >= seconds_for_day:
+        total_time -= seconds_for_day
+        game_date = new_ingame_day(game_date)
+
+    current_date_str = game_date.strftime("%Y-%m-%d")
+    date_text = font.render(f"Game Date: {current_date_str}", True, BLACK)
+    text_width, text_height = date_text.get_size()
+
+
+    #pygame.draw.rect(screen, BLUE, (screen_width//2 - 25, screen_height//2 - 30, 50, 60))
+
+    screen.blit(date_text, (screen_width - text_width, 10))  # Display the date at the top-left corner
+
+    # update the screen display
+    pygame.display.flip()
+
+
 
 # quit Pygame
 pygame.quit()
